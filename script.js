8f7b5f6ea00ed9ebb24077c85553ce7f692ce83d
Amr Ahmed <91067484+imnotamr@users.noreply.github.com>
2025-03-14 11:28:40 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+function showSection(sectionId) {
+    // إخفاء كل الأقسام
+    let sections = document.querySelectorAll('.content-section');
+    sections.forEach(section => {
+        section.style.display = "none";
+    });
+
+    // إظهار القسم المختار
+    document.getElementById(sectionId).style.display = "block";
+}
