06096f70507162592a468ccc10b48091d168e5aa
Amr Ahmed <91067484+imnotamr@users.noreply.github.com>
2025-03-14 11:35:24 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 function showSection(sectionId) {
-    // إخفاء كل الأقسام
     let sections = document.querySelectorAll('.content-section');
     sections.forEach(section => {
         section.style.display = "none";
     });
 
-    // إظهار القسم المطلوب
     document.getElementById(sectionId).style.display = "block";
 }
